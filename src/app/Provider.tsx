ef8806cb3ad9074566e3d608a400449fa2fee984
legcy <legcy143@gmail.com>
2024-01-13 20:37:00 +0530

    check:let's check it

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,10 +1,15 @@
 "use client"
 
-import * as React from "react"
+import React, { useEffect } from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function Provider({ children, ...props }: ThemeProviderProps) {
+  
+    useEffect(() => {
+      console.log("hii everyone")
+    }, [])
+    
     
     return (
         <NextThemesProvider {...props}>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" >
-      <body>
+      <body suppressHydrationWarning={true}>
         <Provider
           attribute="class"
           defaultTheme="system"
