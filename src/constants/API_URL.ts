60bef6be4c2c088b375eea3a3dda661cf2c5b30e
legcy <legcy143@gmail.com>
2024-01-17 16:40:55 +0530

    api adding

--- src/constants/API_URL.ts
+++ src/constants/API_URL.ts
@@ -1,1 +1,2 @@
-export const API_URL = "http://192.168.1.13:5000/api/v1"
+// export const API_URL = "http://192.168.1.13:5000/api/v1"
+export const API_URL = "https://ffbackend-m3ck.onrender.com/api/v1"
